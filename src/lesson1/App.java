0da19ff4379cc3b8376bdb903ed624bc9c046afe
Air Mike <mike@mail.ru>
2019-02-28 15:02:17 +0300

    Практическое задание 1.3

--- src/lesson1/App.java
+++ src/lesson1/App.java
@@ -4,8 +4,8 @@ package lesson1;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static java.lang.System.*;
-import static java.util.Arrays.*;
+import static java.lang.System.out;
+import static java.util.Arrays.asList;
 
 
 public class App
@@ -14,7 +14,7 @@ public class App
   public static <T>
   void replaceTwoElem(T[] array, int index1, int index2)
   {
-    T temp = array[index1];
+	T temp = array[index1];
 	array[index1] = array[index2];
 	array[index2] = temp;
   }
@@ -31,12 +31,13 @@ public class App
 
   public static void main(String... args)
   {
-    Object[] arr = {1,2,3};
+	Object[] arr = {1, 2, 3};
+	out.println("Исходный массив: " + Arrays.toString(arr));
 	replaceTwoElem(arr, 0, 2);
-	out.println(Arrays.toString(arr));
+	out.println("Измененный массив: " + Arrays.toString(arr));
 
 	ArrayList<Object> list = toList(arr);
-	out.println(list);
+	out.println("Список, созданный из массива: " + list);
   }
 
 }
